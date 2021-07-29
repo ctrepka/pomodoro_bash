e2b8a27c6cd7256eda83200f5f5cf0592bd7d417
Christopher Repka <repkachristopher@gmail.com>
2021-07-29 08:15:12 -0500

    beep fixed

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 beep() {
-  $ play -q -n synth 0.1 sin 880 || echo -e "\a" && sleep .15
+	_alarm 800 400 && sleep .20
+}
+
+_alarm() {
+  ( \speaker-test --frequency $1 --test sine )&
+  pid=$!
+  \sleep 0.${2}s
+  \kill -9 $pid
 }
 
 beep_3x() {
@@ -10,19 +17,18 @@ beep_3x() {
 
 work_25m() {
 	beep_3x
-	notify_send "get to work, peasant!"
+	notify-send "get to work, peasant!"
 	sleep 1500
 }
 
 break_5m() {
 	beep_3x
-	notify_send "starting short_break" && sleep 300
+	notify-send "starting short_break" && sleep 300
 }
 
 break_15m() {
 	beep_3x
-	notify_send "starting long_break" && sleep 900
-
+	notify-send "starting long_break" && sleep 900
 }
 
 short_cycle() {
@@ -40,5 +46,5 @@ start_pomo() {
 	short_cycle
 	short_cycle
 	long_cycle
-	notify_send "cycle complete"
+	notify-send "cycle complete"
 }
